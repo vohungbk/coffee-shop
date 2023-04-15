fff3ccc9600a555612fd3220ef4ac14b0df8eb56
Hung Vo Viet <vohungbk@gmail.com>
2023-04-15 23:24:51 +0700

    Add config cors

--- server/index.js
+++ server/index.js
@@ -20,6 +20,9 @@ connectDB();
 const app = express();
 app.use(express.json());
 app.use(cors({ origin: "*", methods: "GET,HEAD,PUT,PATCH,POST,DELETE", preflightContinue: false, optionsSuccessStatus: 204 }));
+app.get("/api/test", (req, res) => {
+  res.json("test OK");
+});
 
 app.use("/api/auth", authRouter);
 app.use("/api/product", productRouter);
